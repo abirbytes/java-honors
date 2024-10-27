beab275165311c2708ad7b69515f3ca4c1110a71
Ahnaf Abir <ahnafhossainabir222@gmail.com>
2024-10-27 08:27:31 -0500

    initiated hello world file

---
+++ 01_hello_world/hello_world.java
@@ -0,0 +1,22 @@
+
+public class hello_world{
+   public static void main(String[] args){
+   // System.out.println("Heloo World");
+   //five data types
+   /*
+   1.integer= a whole number (positive/negative)
+   2.double = decimal numbers(5.2/0.5)
+   3.char = one single charachter('A',' ','2','#')
+   4.boolean = a true or false
+   5.String = more than one charachter(similar to char)
+Ex. "A","Mark","Hello World!","ofejrfjesfriflesjnrf"
+   */
+   // String name = "Abir";
+   int a = 3;
+   // int num=5;//Just made variable
+   System.out.println(a);//sout then press "tab" = shortcut
+   System.out.println("hello"); 
+   
+}
+}
+
